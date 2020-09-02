19b6f8f9e105f4d9a2e390404694a988959d1254
taras-gr <tar.grytsenko@gmail.com>
2020-09-02 19:45:45 +0300

    Added welcomecomponent

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component:WelcomepageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomepageComponent } from './welcomepage/welcomepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomepageComponent
   ],
   imports: [
     BrowserModule,
