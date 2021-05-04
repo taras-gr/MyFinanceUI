fd384ae0b5c5616948d719dc26bf7b64c91f735a
taras-gr <tar.grytsenko@gmail.com>
2021-05-04 09:09:22 +0300

    Added production environment to gitignore

--- src/environments/environment.prod.ts
+++
@@ -1,5 +0,0 @@
-export const environment = {
-  production: true,
-  baseApiUri: 'https://v8vm08v6o2.execute-api.us-east-1.amazonaws.com/Prod/api', // aws lambda
-  staticFilesDir: 'https://myfinance-uiassets-bucket.s3.amazonaws.com' // aws s3 bucket
-};
